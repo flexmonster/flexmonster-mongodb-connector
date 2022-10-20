4e8c23b59a9cfa4926fd69f3657ee2a09b95893a
dima_z <dimazvazhii@gmail.com>
2022-10-20 10:26:43 +0300

    fix the issue with random issues after server restart

--- src/api/MongoAPIManager.ts
+++ src/api/MongoAPIManager.ts
@@ -16,7 +16,6 @@ export class MongoAPIManager implements IDataAPI{
     private _mongoResponseParser: MongoResponseParser;
     private _dataManager: DataManager;
     private _queryBuilder: QueryBuilder;
-    //private _dataLoader: RequestHandler;
     private _schemaCache: {[index: string]: APISchema};
     private _configManager: ConfigManager;
     private _apiVersion: string
@@ -25,13 +24,6 @@ export class MongoAPIManager implements IDataAPI{
 
         this._apiVersion = apiVersion;
         this.initializeComponents(config);
-        // this._mongoQueryManager = new MongoQueryExecutor();
-        // this._mongoResponseParser = MongoResponseParser.getInstance();
-        // //this._cacheManager = LocalDataCache.getInstance();
-        // this._queryBuilder = QueryBuilder.getInstance();
-        // this._dataManager = new DataManager(this._queryBuilder, this._mongoQueryManager);
-        // this._dataLoader = new RequestHandler(this._queryBuilder, this._mongoQueryManager, this._dataManager);
-        // this._schemaCache = {};
     }
 
     /**
@@ -40,19 +32,9 @@ export class MongoAPIManager implements IDataAPI{
      * @param {string} index MongoDB's collection name
      * @return {object} returns schema object
      */
-    public async getSchema(dbo: Db, index: string): Promise<APISchema> {
-        if (typeof index != 'string') throw new Error("Incorrect index format");
-        this._mongoQueryManager.injectDBConnection(dbo);
-        const dbName: string = dbo.databaseName;
-        if (this.getIndexSchema(dbName, index) == null) {
-            //console.log(">>>>>>", index, dbo);
-            let document: any = await this._mongoQueryManager.runShemaQuery(index);
-            this.setIndexSchema(dbName, index ,this._mongoResponseParser.parseShemaFromDocument(document));
-            LoggingManager.log(`Quering database ${dbName} to get schema of index ${index}`);
-        } else {
-            LoggingManager.log(`Getting schema of index ${index} of database ${dbName} from cache`);
-        }
-        return this.getIndexSchema(dbName, index).toJSON();
+    public async getSchema(dbo: Db, index: string): Promise<object> {
+        const apiSchema = await this._getSchema(dbo, index);
+        return apiSchema.toJSON();
     }
 
     /**
@@ -64,10 +46,14 @@ export class MongoAPIManager implements IDataAPI{
      * @return {object}
      */
     public async getMembers(dbo: Db, index: CollectionName, fieldObject: any, pagingObject: PagingInterface): Promise<any> {
-        // let apiRequest: IApiRequest = /*(pagingObject.pageToken != null && this._dataLoader.isRequestRegistered(pagingObject.pageToken))
-        //     ? this._dataLoader.getRegisteredRequest(pagingObject.pageToken) 
-        //     :*/ new MembersApiRequest({index: index, fieldObject: fieldObject, clientQuery: {"members": fieldObject}, db: dbo}, this.getIndexSchema(dbo.databaseName, index))
-        return this._dataManager.getData({index: index, fieldObject: fieldObject["field"], clientQuery: {"members": fieldObject}, db: dbo, schema: this.getIndexSchema(dbo.databaseName, index)}, 
+        const apiSchema = await this._getSchema(dbo, index);
+        return this._dataManager.getData({
+                index: index, 
+                fieldObject: fieldObject["field"], 
+                clientQuery: {"members": fieldObject}, 
+                db: dbo, 
+                schema: apiSchema
+            }, 
             RequestType.MEMBERS_REQUEST, pagingObject);
     }
 
@@ -79,35 +65,63 @@ export class MongoAPIManager implements IDataAPI{
      * @param {number} page
      * @return {object}
      */
-    public async getSelectResult(dbo: Db, index: CollectionName, query: any, pagingObject: PagingInterface) {
+    public async getSelectResult(dbo: Db, index: CollectionName, query: any, pagingObject: PagingInterface): Promise<any> {
 
         let response = null;
+        const apiSchema = await this._getSchema(dbo, index);
 
         if (query["aggs"] != null && query["fields"] == null) {
-
-            // let apiRequest: IApiRequest = /* (pagingObject.pageToken != null && this._dataLoader.isRequestRegistered(pagingObject.pageToken))
-            //     ? this._dataLoader.getRegisteredRequest(pagingObject.pageToken) 
-            //     :*/ new AggregationApiRequest({index: index, clientQuery: query, db: dbo}, this.getIndexSchema(dbo.databaseName, index));
-            response = this._dataManager.getData({index: index, clientQuery: query, db: dbo, schema: this.getIndexSchema(dbo.databaseName, index)}, 
+            
+            response = this._dataManager.getData({
+                    index: index,
+                    clientQuery: query,
+                    db: dbo,
+                    schema: apiSchema
+                }, 
                 RequestType.AGGREGATION_REQUEST, pagingObject);
 
         } else if (query["aggs"] == null && query["fields"] != null) {//drill-through
 
-            // let apiRequest: IApiRequest = /* (pagingObject.pageToken != null && this._dataLoader.isRequestRegistered(pagingObject.pageToken))
-            //     ? this._dataLoader.getRegisteredRequest(pagingObject.pageToken) 
-            //     :*/ new DrillThroughApiRequest({index: index, clientQuery: query, db: dbo}, this.getIndexSchema(dbo.databaseName, index))
-            response = this._dataManager.getData({index: index, clientQuery: query, db: dbo, schema: this.getIndexSchema(dbo.databaseName, index)}, 
+            response = this._dataManager.getData({
+                    index: index,
+                    clientQuery: query,
+                    db: dbo,
+                    schema: apiSchema
+                }, 
                 RequestType.DRILLTHROUGH_REQUEST, pagingObject);
         } else if (query["aggs"] != null && query["fields"] != null) {// flat-form
 
-            // let apiRequest: IApiRequest = /*(pagingObject.pageToken != null && this._dataLoader.isRequestRegistered(pagingObject.pageToken))
-            //     ? this._dataLoader.getRegisteredRequest(pagingObject.pageToken) 
-            //     :*/ new FlatApiRequest({index: index, clientQuery: query, db: dbo}, this.getIndexSchema(dbo.databaseName, index))
-            response = this._dataManager.getData({index: index, clientQuery: query, db: dbo, schema: this.getIndexSchema(dbo.databaseName, index)}, 
+            response = this._dataManager.getData({
+                    index: index,
+                    clientQuery: query,
+                    db: dbo,
+                    schema: apiSchema
+                }, 
                 RequestType.FLAT_REQUEST, pagingObject);
         }
 
         return response;
+    }
+
+    /**
+     * Returns the schema 
+     * @method
+     * @param {string} index MongoDB's collection name
+     * @return {APISchema} returns APISchema object
+     */
+    private async _getSchema(dbo: Db, index: string): Promise<APISchema> {
+        if (typeof index != 'string') throw new Error("Incorrect index format");
+        this._mongoQueryManager.injectDBConnection(dbo);
+        const dbName: string = dbo.databaseName;
+        if (this._schemaCache[`${dbName}_${index}`] == null) {
+            //console.log(">>>>>>", index, dbo);
+            let document: any = await this._mongoQueryManager.runShemaQuery(index);
+            this._schemaCache[`${dbName}_${index}`] = this._mongoResponseParser.parseShemaFromDocument(document);
+            LoggingManager.log(`Quering database ${dbName} to get schema of index ${index}`);
+        } else {
+            LoggingManager.log(`Getting schema of index ${index} of database ${dbName} from cache`);
+        }
+        return this._schemaCache[`${dbName}_${index}`];
     }
 
     private initializeComponents(config: ConfigInterface): void {
@@ -116,20 +130,12 @@ export class MongoAPIManager implements IDataAPI{
         this._mongoResponseParser = MongoResponseParser.getInstance();
         new LoggingManager(this._configManager.currentConfig.logsEnabled);
 
-        //this._cacheManager = LocalDataCache.getInstance();
         this._queryBuilder = QueryBuilder.getInstance();
         this._dataManager = new DataManager(this._queryBuilder, this._mongoQueryManager);
-        //this._dataLoader = new RequestHandler(this._queryBuilder, this._mongoQueryManager, this._dataManager);
+
         this._schemaCache = {};
         LoggingManager.log("Version:", this._apiVersion);
         LoggingManager.log("Started with the following config", this._configManager);
     } 
 
-    private getIndexSchema(dbName: string, index: string): APISchema {
-        return this._schemaCache[`${dbName}_${index}`];
-    }
-
-    private setIndexSchema(dbName: string, index: string, apiSchema: APISchema): void {
-        this._schemaCache[`${dbName}_${index}`] = apiSchema;
-    }
 }
